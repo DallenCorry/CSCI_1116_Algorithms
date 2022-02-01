1ecae8a373d1395d300c703a3128c3b42e43329a
Dallen Corry <dallenrobert@gmail.com>
2022-02-01 10:08:26 -0700

    Change name of Class

--- Exercise20_3.java
+++ Exercise20_3.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Exercise08_37 {
+public class Exercise20_3 {
   public static void main(String[] args) {
     String[][] stateCapital = {
       {"Alabama", "Montgomery"},
