e8a054288f1482f7a7567f8b9bf6554b6a86336d
DallenCorry <89269364+DallenCorry@users.noreply.github.com>
2022-02-24 14:59:22 -0700

    put creation of hash table in method

--- Ch21/Exercise21_09.java
+++ Ch21/Exercise21_09.java
@@ -3,85 +3,91 @@ package capitals;
 import java.util.*;
 
 public class Exercise21_09 {
-  public static void main(String[] args) {
-    String[][] stateCapital = {
-      {"Alabama", "Montgomery"},
-      {"Alaska", "Juneau"},
-      {"Arizona", "Phoenix"},
-      {"Arkansas", "Little Rock"},
-      {"California", "Sacramento"},
-      {"Colorado", "Denver"},
-      {"Connecticut", "Hartford"},
-      {"Delaware", "Dover"},
-      {"Florida", "Tallahassee"},
-      {"Georgia", "Atlanta"},
-      {"Hawaii", "Honolulu"},
-      {"Idaho", "Boise"},
-      {"Illinois", "Springfield"},
-      {"Indiana", "Indianapolis"},
-      {"Iowa", "Des Moines"},
-      {"Kansas", "Topeka"},
-      {"Kentucky", "Frankfort"},
-      {"Louisiana", "Baton Rouge"},
-      {"Maine", "Augusta"},
-      {"Maryland", "Annapolis"},
-      {"Massachusettes", "Boston"},
-      {"Michigan", "Lansing"},
-      {"Minnesota", "Saint Paul"},
-      {"Mississippi", "Jackson"},
-      {"Missouri", "Jefferson City"},
-      {"Montana", "Helena"},
-      {"Nebraska", "Lincoln"},
-      {"Nevada", "Carson City"},
-      {"New Hampshire", "Concord"},
-      {"New Jersey", "Trenton"},
-      {"New York", "Albany"},
-      {"New Mexico", "Santa Fe"},
-      {"North Carolina", "Raleigh"},
-      {"North Dakota", "Bismarck"},
-      {"Ohio", "Columbus"},
-      {"Oklahoma", "Oklahoma City"},
-      {"Oregon", "Salem"},
-      {"Pennsylvania", "Harrisburg"},
-      {"Rhode Island", "Providence"},
-      {"South Carolina", "Columbia"},
-      {"South Dakota", "Pierre"},
-      {"Tennessee", "Nashville"},
-      {"Texas", "Austin"},
-      {"Utah", "Salt Lake City"},
-      {"Vermont", "Montpelier"},
-      {"Virginia", "Richmond"},
-      {"Washington", "Olympia"},
-      {"West Virginia", "Charleston"},
-      {"Wisconsin", "Madison"},
-      {"Wyoming", "Cheyenne"}
-    };
-    
-    HashMap<String,String> capitalMap = new HashMap<String,String>();
-    for(String[] arr:stateCapital) {
-    	capitalMap.put(arr[0], arr[1]);
-    }
-    
-    Scanner input = new Scanner(System.in);
-    
-    int correctCount = 0;
+	public static int correctCount = 0;
+	public static void main(String[] args) {
+		HashMap<String,String> capitalMap = getMap();
 
-    for (int i = 0; i < stateCapital.length; i++) {
-      // Prompt the user with a question
-      System.out.print("What is the capital of " + stateCapital[i][0] + "? ");
-      String capital = input.nextLine().trim().toLowerCase();
-      
-      if (capital.toLowerCase().equals(stateCapital[i][1].toLowerCase())) {
-        System.out.println("Your answer is correct");
-        correctCount++;
-      }
-      else
-        System.out.println("The correct answer should be " + stateCapital[i][1]);
-    }
-    
-    input.close();
-    
-    System.out.println("The correct count is " + correctCount);
-  }
+		Scanner input = new Scanner(System.in);
+
+		capitalMap.forEach((k,v)-> {
+			System.out.print("What is the capital of " + k + "? ");
+			String capital = input.nextLine().trim().toLowerCase();
+			if (capital.toLowerCase().equals(v.toLowerCase())) {
+				System.out.println("Correct!");
+				increaseScore(1);
+			} else {
+				System.out.println("The correct answer should be "+v);
+			}
+		});
+		
+		input.close();
+
+		System.out.println("\nThe correct count is " + correctCount);
+	}
+
+	private static void increaseScore(int amount) {
+		correctCount++;
+	}
+
+	private static HashMap<String,String> getMap() {
+		String[][] stateCapital = {
+				{"Alabama", "Montgomery"},
+				{"Alaska", "Juneau"},
+				{"Arizona", "Phoenix"},
+				{"Arkansas", "Little Rock"},
+				{"California", "Sacramento"},
+				{"Colorado", "Denver"},
+				{"Connecticut", "Hartford"},
+				{"Delaware", "Dover"},
+				{"Florida", "Tallahassee"},
+				{"Georgia", "Atlanta"},
+				{"Hawaii", "Honolulu"},
+				{"Idaho", "Boise"},
+				{"Illinois", "Springfield"},
+				{"Indiana", "Indianapolis"},
+				{"Iowa", "Des Moines"},
+				{"Kansas", "Topeka"},
+				{"Kentucky", "Frankfort"},
+				{"Louisiana", "Baton Rouge"},
+				{"Maine", "Augusta"},
+				{"Maryland", "Annapolis"},
+				{"Massachusettes", "Boston"},
+				{"Michigan", "Lansing"},
+				{"Minnesota", "Saint Paul"},
+				{"Mississippi", "Jackson"},
+				{"Missouri", "Jefferson City"},
+				{"Montana", "Helena"},
+				{"Nebraska", "Lincoln"},
+				{"Nevada", "Carson City"},
+				{"New Hampshire", "Concord"},
+				{"New Jersey", "Trenton"},
+				{"New York", "Albany"},
+				{"New Mexico", "Santa Fe"},
+				{"North Carolina", "Raleigh"},
+				{"North Dakota", "Bismarck"},
+				{"Ohio", "Columbus"},
+				{"Oklahoma", "Oklahoma City"},
+				{"Oregon", "Salem"},
+				{"Pennsylvania", "Harrisburg"},
+				{"Rhode Island", "Providence"},
+				{"South Carolina", "Columbia"},
+				{"South Dakota", "Pierre"},
+				{"Tennessee", "Nashville"},
+				{"Texas", "Austin"},
+				{"Utah", "Salt Lake City"},
+				{"Vermont", "Montpelier"},
+				{"Virginia", "Richmond"},
+				{"Washington", "Olympia"},
+				{"West Virginia", "Charleston"},
+				{"Wisconsin", "Madison"},
+				{"Wyoming", "Cheyenne"}
+		};
+
+		HashMap<String,String> capitalMap = new HashMap<String,String>();
+		for(String[] arr:stateCapital) {
+			capitalMap.put(arr[0], arr[1]);
+		}
+		return capitalMap;
+	}
 }
 
