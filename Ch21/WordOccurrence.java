7fca7a1d185bb8dc4220fb93cca503543a818e25
DallenCorry <89269364+DallenCorry@users.noreply.github.com>
2022-02-24 14:20:00 -0700

    Create WordOccurrence.java

---
+++ Ch21/WordOccurrence.java
@@ -0,0 +1,19 @@
+public class WordOccurrence implements Comparable<WordOccurrence> {
+	String word;
+	Integer count;
+	
+	WordOccurrence (String word, Integer count) {
+		this.word = word;
+		this.count = count;
+	}
+	
+	@Override
+	public int compareTo(WordOccurrence object2) {
+			return count-object2.count;
+	}
+	
+	@Override
+	public String toString() {
+		return word + count;
+	}
+}
