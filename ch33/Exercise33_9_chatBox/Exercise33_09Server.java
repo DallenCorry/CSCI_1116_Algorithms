7bde510fe1f6a3250209c8e5d4e3123c54506b68
DallenCorry <89269364+DallenCorry@users.noreply.github.com>
2022-05-05 11:53:47 -0600

    Create Exercise33_09Server.java

---
+++ ch33/Exercise33_9_chatBox/Exercise33_09Server.java
@@ -0,0 +1,57 @@
+import javax.swing.event.*;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Exercise33_09Server extends Application {
+  private TextArea taServer = new TextArea();
+  private TextArea taClient = new TextArea();
+ 
+  @Override // Override the start method in the Application class
+  public void start(Stage primaryStage) {
+    taServer.setWrapText(true);
+    taServer.setEditable(false);
+    taClient.setWrapText(true);
+    //taClient.setDisable(true);
+
+    BorderPane pane1 = new BorderPane();
+    pane1.setTop(new Label("History"));
+    pane1.setCenter(new ScrollPane(taServer));
+    BorderPane pane2 = new BorderPane();
+    pane2.setTop(new Label("New Message"));
+    pane2.setCenter(new ScrollPane(taClient));
+    
+    VBox vBox = new VBox(5);
+    vBox.getChildren().addAll(pane1, pane2);
+    
+    taClient.setOnKeyReleased(e -> {
+    	if (e.getCode().equals(KeyCode.ENTER)) {
+	    	taServer.appendText("S: "+taClient.getText());
+	    	taClient.clear();
+    	}
+    });
+
+    // Create a scene and place it in the stage
+    Scene scene = new Scene(vBox, 200, 200);
+    primaryStage.setTitle("Exercise31_09Server"); // Set the stage title
+    primaryStage.setScene(scene); // Place the scene in the stage
+    primaryStage.show(); // Display the stage
+
+    //Access data from the client.
+  }
+
+  /**
+   * The main method is only needed for the IDE with limited
+   * JavaFX support. Not needed for running from the command line.
+   */
+  public static void main(String[] args) {
+    launch(args);
+  }
+}
