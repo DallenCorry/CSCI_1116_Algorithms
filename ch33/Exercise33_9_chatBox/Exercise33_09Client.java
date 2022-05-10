fb1de10530c8579c6582055574651294e361cad1
DallenCorry <89269364+DallenCorry@users.noreply.github.com>
2022-05-10 10:50:31 -0600

    finished?

--- ch33/Exercise33_9_chatBox/Exercise33_09Client.java
+++ ch33/Exercise33_9_chatBox/Exercise33_09Client.java
@@ -1,9 +1,14 @@
+import java.io.*;
+import java.net.*;
+
 import javafx.application.Application;
+import javafx.application.Platform;
 import static javafx.application.Application.launch;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -11,10 +16,16 @@ import javafx.stage.Stage;
 public class Exercise33_09Client extends Application {
   private TextArea taServer = new TextArea();
   private TextArea taClient = new TextArea();
+  private Socket clientSocket;
+  private int port = 5000;
+  DataInputStream in = null;
+  DataOutputStream out = null;
  
   @Override // Override the start method in the Application class
   public void start(Stage primaryStage) {
     taServer.setWrapText(true);
+    taServer.setEditable(false);
+    taServer.setStyle("-fx-text-fill: Red;");
     taClient.setWrapText(true);
     //taServer.setDisable(true);
 
@@ -28,13 +39,52 @@ public class Exercise33_09Client extends Application {
     VBox vBox = new VBox(5);
     vBox.getChildren().addAll(pane1, pane2);
 
+    taServer.prefWidthProperty().bind(vBox.widthProperty().subtract(20));
+    taClient.prefWidthProperty().bind(vBox.widthProperty().subtract(20));
+    taServer.prefHeightProperty().bind(vBox.heightProperty().divide(2).subtract(25));
+    taClient.prefHeightProperty().bind(vBox.heightProperty().divide(2).subtract(25));
+    
     // Create a scene and place it in the stage
-    Scene scene = new Scene(vBox, 200, 200);
+    Scene scene = new Scene(vBox, 300, 300);
     primaryStage.setTitle("Exercise31_09Client"); // Set the stage title
     primaryStage.setScene(scene); // Place the scene in the stage
     primaryStage.show(); // Display the stage
+    
 
-    //Access the server
+
+    new Thread(() -> {
+	    try {
+		    taClient.setOnKeyReleased(e -> {
+		    	if (e.getCode().equals(KeyCode.ENTER)) {
+		    		//send to the server
+		    		try {
+		    			String temp = "C: "+taClient.getText();
+						out.writeChars(temp);
+						out.flush();
+						taServer.appendText("C: "+taClient.getText());
+			    		taClient.clear();
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					}
+			    	
+		    	}
+		    });
+	    	
+	    	clientSocket = new Socket("localhost", port);
+	    	in = new DataInputStream(clientSocket.getInputStream());
+	    	out = new DataOutputStream(clientSocket.getOutputStream());
+		   
+		  while(true) {
+			    String txt = in.readUTF();
+
+		    	Platform.runLater(() ->
+		    			taServer.appendText(txt));
+		    }
+	    	
+	    } catch(IOException e) {
+	    	e.printStackTrace();
+	    }
+  	}).start();
   }
 
   /**
