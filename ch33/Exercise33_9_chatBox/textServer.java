dde5bf164d1028ad0f36143da70e82e95068c1df
DallenCorry <89269364+DallenCorry@users.noreply.github.com>
2022-05-10 11:55:31 -0600

    Update textServer.java

--- ch33/Exercise33_9_chatBox/textServer.java
+++ ch33/Exercise33_9_chatBox/textServer.java
@@ -1,4 +1,8 @@
+import java.io.*;
+import java.net.*;
+
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -6,12 +10,14 @@ import javafx.scene.control.TextArea;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
-public class textServer extends Application{
-	
+public class textServer extends Application {
+	TextArea taServerField;
+	private int clientID = 0;
+	private int port = 8000;
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
-		TextArea taServerField = new TextArea();
+		taServerField = new TextArea();
 		Button btReset = new Button("Reset Server");
 		VBox pane = new VBox(taServerField, btReset);
 
@@ -28,8 +34,54 @@ public class textServer extends Application{
 		Scene scene = new Scene(pane);
 		primaryStage.setScene(scene);
 		primaryStage.show();
+		
+		new Thread(() -> {
+			try {
+				ServerSocket serverSocket = new ServerSocket(port);
+				taServerField.appendText("MultiThreadServer started at "+ System.nanoTime());
+				while (true) {
+					Socket socket = serverSocket.accept();
+					clientID++;
+					Platform.runLater( () -> {
+						taServerField.appendText("Starting thread for client " + clientID);
+						InetAddress inetAddress = socket.getInetAddress();
+						taServerField.appendText("Client " + clientID + "'s host name is "+ inetAddress.getHostName() + "\n");
+						taServerField.appendText("Client " + clientID + "'s IP Address is"+ inetAddress.getHostAddress() + "\n");
+					});
+					new Thread(new ThreadClass(socket)).start();
+				}
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		});
 	}
+	
+	class ThreadClass implements Runnable {
+		private Socket socket;
+		ThreadClass(Socket socket) {
+			this.socket = socket;
+		}
+		
+		//Override
+		public void run() {
+            try {
+            	DataInputStream inputFromClient = new DataInputStream(socket.getInputStream());
+            	DataOutputStream outputToClient = new DataOutputStream(socket.getOutputStream());
 
+            	while (true) {
+            		String txt = inputFromClient.readUTF();
+    		    	
+    		    	Platform.runLater(() -> {
+    		    		taServerField.appendText(txt);
+    		    	});
+            	}
+            }
+            catch(IOException ex) {
+            	ex.printStackTrace();
+            }
+		}
+	}
 	public static void main(String[] args) {
 		Application.launch(args);
 	}
